02dd5d27a1a6b3ac8cd364d63033774847cf923e
Filip Jinstrand <filip@jinstrand.se>
2019-09-20 10:57:41 +0200

    Done with step one

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace Vektorer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] tal = new int[5];
+
+            Console.WriteLine("Skriv in fem heltal");
+
+            for (int i = 0; i < tal.Length; i++)
+            {
+                Console.Write("Skriv ett tal: ");
+                tal[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            int samman = 0;
+
+            for (int i = 0; i < tal.Length; i++)
+            {
+                Console.WriteLine(tal[i]);
+
+                samman += tal[i];
+
+            }
+            
+            Console.WriteLine(samman);
+
         }
     }
 }
